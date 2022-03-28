95ad7b82187a0b9ea64d76681b6964028a097411
Matyas Nagy <nagymatyas87@gmail.com>
2022-03-28 22:44:57 +0200

    ScraperController has been implemented

---
+++ TheBigImdbQuest/TheBigImdbQuest/ScraperController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections.Generic;
+
+namespace TheBigImdbQuest
+{
+    class ScraperController
+    {
+        readonly string baseUrl = @"https://www.imdb.com/";
+
+        public async Task<string> GetUrlAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            var result = client.GetStringAsync(url);
+            return await result;
+        }
+
+        public async Task<List<string>> GetUrlAsync(string[] urlPostfix)
+        {
+            List<Task<string>> tasks = new List<Task<string>>();
+
+            foreach (var post in urlPostfix)
+            {
+                async Task<string> func()
+                {
+                    return await GetUrlAsync(baseUrl + urlPostfix);
+                }
+
+                tasks.Add(func());
+            }
+
+            await Task.WhenAll(tasks);
+
+            List<string> responses = new List<string>();
+
+            foreach (var task in tasks)
+            {
+                responses.Add(await task);
+            }
+
+            return await Task.FromResult(responses);
+        }
+    }
+}
