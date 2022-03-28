66d0983912f46a213c37308996b42e8c235968e6
Matyas Nagy <nagymatyas87@gmail.com>
2022-03-28 22:44:12 +0200

    data structure of Movies has been created

---
+++ TheBigImdbQuest/TheBigImdbQuest/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBigImdbQuest
+{
+    public class Movie
+    {
+        public string Title { get; }
+        public double OriginalRating { get; }
+        public double RecalculatedRating { get; }
+        public int NrOfRatings { get; }
+        public int NrOfOscars { get; }
+    }
+}
