1aafe44b185af1b9d900d179f08092db53ca125f
Matyas Nagy <nagymatyas87@gmail.com>
2022-03-28 20:19:53 +0200

    OscarCalculator has been implemented

---
+++ TheBigImdbQuest/TheBigImdbQuest/OscarCalculator.cs
@@ -0,0 +1,33 @@
+﻿namespace TheBigImdbQuest
+{
+    public static class OscarCalculator
+    {
+        public static double GetOscarRewards(int nrOfOscars)
+        {
+            double reward;
+
+            if(nrOfOscars == 0)
+            {
+                reward = 0;
+            }
+            else if(nrOfOscars <= 2)
+            {
+                reward = 0.3;
+            }
+            else if (nrOfOscars <= 5)
+            {
+                reward = 0.5;
+            }
+            else if (nrOfOscars <= 10)
+            {
+                reward = 1.0;
+            }
+            else
+            {
+                reward = 1.5;
+            }
+
+            return reward;
+        }
+    }
+}
---
+++ TheBigImdbQuest/TheBigImdbQuest_Test/OscarCalculator_Test.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using TheBigImdbQuest;
+
+namespace TheBigImdbQuest_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase( 0, 0.0)]
+        [TestCase( 1, 0.3)]
+        [TestCase( 2, 0.3)]
+        [TestCase( 3, 0.5)]
+        [TestCase( 4, 0.5)]
+        [TestCase( 5, 0.5)]
+        [TestCase( 6, 1.0)]
+        [TestCase( 7, 1.0)]
+        [TestCase( 8, 1.0)]
+        [TestCase( 9, 1.0)]
+        [TestCase(10, 1.0)]
+        [TestCase(11, 1.5)]
+        public void Test_Pass(int nrOfOscars, double reward)
+        {
+            Assert.AreEqual(reward, OscarCalculator.GetOscarRewards(nrOfOscars));
+        }
+    }
+}
