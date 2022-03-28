f916c2d6f69e037f713127f60db11c4986d9c27b
Matyas Nagy <nagymatyas87@gmail.com>
2022-03-28 20:56:18 +0200

    Review Penalizer has been implemented

---
+++ TheBigImdbQuest/TheBigImdbQuest/ReviewPenalizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBigImdbQuest
+{
+    public class ReviewPenalizer
+    {
+        private readonly double penaltyRate;
+
+        public ReviewPenalizer(double deviation, double deduction)
+        {
+            this.penaltyRate = deviation / deduction;
+        }
+
+        public double GetReviewPenalty(int maxNrOfReviews, int actualNrOfReviews)
+        {
+            return Math.Floor((maxNrOfReviews - actualNrOfReviews) / penaltyRate * 10) / 10;
+        }
+    }
+}
---
+++ TheBigImdbQuest/TheBigImdbQuest_Test/ReviewPenalizer_Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TheBigImdbQuest;
+
+namespace TheBigImdbQuest_Test
+{
+    public class ReviewPenalizer_Test
+    {
+        const double Deviation = 100000;
+        const double Deduction = 0.1;
+        ReviewPenalizer penalizer;
+
+        [SetUp]
+        public void Setup()
+        {
+            penalizer = new ReviewPenalizer(Deviation, Deduction);
+        }
+
+        [Test]
+        [TestCase(2456123, 1258369, 1.1)]
+        public void GivenAmountOfDevationIsReceived_ExpectToCalculateScorePenalty(int maxNrOfReviews, int actualNrOfReviews, double penalty)
+        {
+            Assert.AreEqual(penalty, penalizer.GetReviewPenalty(maxNrOfReviews, actualNrOfReviews));
+        }
+    }
+}
